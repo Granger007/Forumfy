08c3a45d1c3731b4e19911ef8fc6a0e0c5cba55a
kwazzy-coder <samrudhanarendra2005@gmail.com>
2024-11-16 15:25:47 +0530

    Updated Sidebar.jsx styling

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MdHomeFilled } from "react-icons/md";
-import { IoNotifications } from "react-icons/io5";
+import { IoNotifications, IoTrendingUp} from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
@@ -48,15 +48,15 @@ const Sidebar = () => {
 			<div className="flex flex-col h-[calc(100vh-64px)] border-r border-gray-700">
 				<ul className="flex flex-col gap-3 mt-4">
 					<li className="flex justify-center md:justify-start">
-						<Link to="/" className="flex gap-3 items-center  hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer">
-							<MdHomeFilled className="w-8 h-8 " />
-							<span className="text-lg hidden md:block">Home</span>
+						<Link to="/" className="flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer">
+							<MdHomeFilled className="w-8 h-8 text-secondary" />
+							<span className="text-lg text-secondary hidden md:block">Home</span>
 						</Link>
 					</li>
 					<li className="flex justify-center md:justify-start">
 						<Link to="/notifications" className="flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer">
-							<IoNotifications className="w-6 h-6" />
-							<span className="text-lg hidden md:block">Notifications</span>
+							<IoNotifications className="w-6 h-6 text-secondary" />
+							<span className="text-lg text-secondary hidden md:block">Notifications</span>
 						</Link>
 					</li>
 					<li className="flex justify-center md:justify-start">
@@ -68,8 +68,8 @@ const Sidebar = () => {
 					{authUser && (
 						<li className="flex justify-center md:justify-start">
 							<Link to={`/profile/${authUser.username}`} className="flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer">
-								<FaUser className="w-6 h-6" />
-								<span className="text-lg hidden md:block">Profile</span>
+								<FaUser className="w-6 h-6 text-secondary" />
+								<span className="text-lg text-secondary hidden md:block">Profile</span>
 							</Link>
 						</li>
 					)}
