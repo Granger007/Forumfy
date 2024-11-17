bd375351234e010eeaa7da73a48156d4c201dad7
kwazzy-coder <samrudhanarendra2005@gmail.com>
2024-11-17 18:13:43 +0530

    Updated LoginPage.jsx

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-//import XSvg from "../../../components/svgs/X";
-
 import { FaUser } from "react-icons/fa";
 import { MdPassword } from "react-icons/md";
 
@@ -58,12 +56,8 @@ const LoginPage = () => {
 
 	return (
 		<div className='max-w-screen-xl mx-auto flex h-screen'>
-			{/* <div className='flex-1 hidden lg:flex items-center  justify-center'>
-				//<XSvg className='lg:w-2/3 fill-white' />
-			</div> */}
 			<div className='flex-1 flex flex-col justify-center items-center'>
 				<form className='flex gap-4 flex-col' onSubmit={handleSubmit}>
-					{/* <XSvg className='w-24 lg:hidden fill-white' /> */}
 					<h1 className='text-4xl font-extrabold text-white'>{"Let's"} go.</h1>
 					<label className='input input-bordered rounded flex items-center gap-2'>
 						<FaUser />
@@ -94,9 +88,9 @@ const LoginPage = () => {
 					{isError && <p className='text-red-500'>{error.message}</p>}
 				</form>
 				<div className='flex flex-col gap-2 mt-4'>
-					<p className='text-white text-lg'>{"Don't"} have an account?</p>
+					<p className='text-primary text-lg'>{"Don't"} have an account?</p>
 					<Link to='/signup'>
-						<button className='btn rounded-full btn-white text-white btn-outline w-full'>Sign up</button>
+						<button className='btn rounded-full btn-primary text-white btn-outline w-full'>Sign up</button>
 					</Link>
 				</div>
 			</div>
